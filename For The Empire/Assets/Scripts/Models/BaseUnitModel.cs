c7d24f30ccb2273a1920a0da6f94c7b614f242e5
yangsikk <chansik23@hanmail.net>
2023-08-25 15:29:48 +0900

    유닛 에셋 추가

--- For The Empire/Assets/Scripts/Models/BaseUnitModel.cs
+++ For The Empire/Assets/Scripts/Models/BaseUnitModel.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
 
-public class BaseUnitModel : MonoBehaviour, ILife, ITarget{
+public abstract class BaseUnitModel : MonoBehaviour, ILife, ITarget{
     public LifeAbility life {get; set;}
     public bool isTargetable {get; set;} = true;
 }
